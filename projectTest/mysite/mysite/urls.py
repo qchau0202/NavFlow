5855ac9f9c2170561afe68fc549afc2cad7f5969
iamchouty <quocchau4729@gmail.com>
2024-10-07 20:57:02 +0700

    Fixed URL path

--- projectTest/mysite/mysite/urls.py
+++ projectTest/mysite/mysite/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
+def index(request):
+    return HttpResponse("This is the homepage.")
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
-    # path("admin/", admin.site.urls),
+    path('', index),  
+    path('polls/', include('polls.urls')),
 ]
